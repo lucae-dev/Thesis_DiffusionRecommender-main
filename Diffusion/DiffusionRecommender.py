e991d4b415ed2516416b02f57ad85fcac6ed7548
lucaortolomo <l.ortolomo@gmail.com>
2024-02-04 23:47:48 +0100

    ..

--- Diffusion/DiffusionRecommender.py
+++ Diffusion/DiffusionRecommender.py
@@ -93,9 +93,9 @@ class _GaussianDiffusionModel(nn.Module):
         a_signed = self.noise_schedule.get_a_signed(t)
         print("a_signed shape: ")
         print(a_signed.shape)
+        batch_size = len(t)
         print("a_sized_reshaped: ")
         print(a_signed.reshape(batch_size,1).shape)
-        batch_size = len(t)
         x_noisy = torch.sqrt(a_signed).reshape(batch_size, 1)*x_start + torch.sqrt(1 - a_signed).reshape(batch_size, 1) * gaussian_noise
 
         return x_noisy
