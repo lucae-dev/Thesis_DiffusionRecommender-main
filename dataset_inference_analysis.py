e2876721676a14688630b3678503adc15a6943b9
lucaortolomo <l.ortolomo@gmail.com>
2024-06-15 11:18:59 +0200

    ..

--- dataset_inference_analysis.py
+++ dataset_inference_analysis.py
@@ -231,9 +231,12 @@ if __name__ == '__main__':
             
             # Fit the model
             if model:    
-
+                noise_timestamps = 30
                 for inference_timestamp in range(0, 110, 10):
-                    hyperparams['noise_timesteps'] = inference_timestamp + 2
+                    if (inference_timestamp >= noise_timestamps):
+                        noise_timestamps = noise_timestamps + 30
+                        
+                    hyperparams['noise_timesteps'] = noise_timestamps
                     recommender_instance.fit(**hyperparams)
                     if inference_timestamp == 0:
                         inference_timestamp = 1
